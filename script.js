bbb1fea8cf974e066a5d296c9e542b93ba1b2d0d
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-08-02 16:26:19 +0000

    access the page body using document.querySelector() and store it as  a variable

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* The following function will run every time the dark mode button is clicked. */
 
 function toggleDarkMode() {
+  // access the page body using document.querySelector() and store it as  a variable
+  const pageBody= document.querySelector("body");
+  // access the dark mode buttom using document.getELementByID() and store is it as a variable
   
 
 }
