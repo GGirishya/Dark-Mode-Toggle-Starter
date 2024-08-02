9a8962a114a7f874fa98f0e9c47f5f8e42823146
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-08-02 16:33:38 +0000

    access the dark mode buttom using document.getELementByID() or document.querrySelector()

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 function toggleDarkMode() {
   // access the page body using document.querySelector() and store it as  a variable
   const pageBody= document.querySelector("body");
-  // access the dark mode buttom using document.getELementByID() or document.getElelment byID and store is it as a variable
-  const button = document.getElementById("#dark-mode-button")
+  // access the dark mode buttom using document.getELementByID() or document.querrySelector() and store is it as a variable
+  const button = document.querySelector("#dark-mode-button")
 
   // add the dark css class to all visual page elements
   pageBody.classList.toggle("dark");
-  // 
+  // IF 
 
 }
